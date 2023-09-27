3313a623a122da35de0d04f4fa968edb50787b07
brunamy <bm6952@gmail.com>
2023-09-27 16:14:26 -0300

    Primeiro commit

---
+++ teste.php
@@ -0,0 +1,5 @@
+<?php
+echo "<center><h1>Página gerada no servidor 0034</h1></center>";
+echo "<center><h1>Seja bem-vindo a página local do servidor WEB: instalado por 0034</h1></center>";
+phpinfo();
+?>
